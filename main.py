ed8842eeba01158425dcfa9c57a593b6a52019ed
ronaldfutebol10-debug <ronaldfutebol10@gmail.com>
2026-06-13 13:37:06 -0300

    update function

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ async def upload_csv(file: UploadFile = File(...),authorization: str = Header(..
     1:'Janeiro',2:'Fevereiro',3:'Março',4:'Abril',5:'Maio',6:'Junho',
     7:'Julho',8:'Agosto',9:'Setembro',10:'Outubro',11:'Novembro',12:'Dezembro'
     }
-    df['Ano de Compra'] = df['Data'].dt.year
+    
     df['Mês de Compra'] = df['Data'].dt.month.map(meses)
 
     map_df = mapa_despesas()
