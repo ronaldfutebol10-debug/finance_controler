b5e1c7b9c216460f530a6f49299a6e5d94d1644c
ronaldfutebol10-debug <ronaldfutebol10@gmail.com>
2026-08-31 13:39:02 -0300

    FN BACKEND - UPDATE PACKAGES

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 
-SUPABASE_URL = os.getenv['SUPABASE_URL']
-SUPABASE_KEY = os.getenv['SUPABASE_KEY']
+load_dotenv()
+
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 supabase = create_client(supabase_key=SUPABASE_KEY,supabase_url=SUPABASE_URL)
